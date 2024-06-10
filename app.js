08ff0788d7bcb4c0849a05953f96b13d036bdaed
Moisés Madeira <msalcmd@gmail.com>
2024-06-10 16:16:35 -0300

    captura de erros

--- app.js
+++ app.js
@@ -1,34 +1,61 @@
 import express from "express";
 import bodyParser from "body-parser";
-import cors from "cors"
-import { getAluno, getAlunos, createAluno } from './database.js'
+import cors from "cors";
+import { getAluno, getAlunos, createAluno } from './database.js';
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
-app.use(cors())
+// Endpoint raiz
+app.get("/", (req, res) => {
+    res.status(200).send("Servidor está funcionando corretamente.");
+});
 
 // Endpoint de healthcheck
 app.get("/health", (req, res) => {
     res.status(200).send("Servidor está funcionando corretamente.");
 });
 
-app.get("/alunos", async (req,res) => {
-    const alunos = await getAlunos();
-    res.send(alunos);
+app.get("/alunos", async (req, res, next) => {
+    try {
+        const alunos = await getAlunos();
+        res.send(alunos);
+    } catch (error) {
+        console.error('Erro ao buscar alunos:', error);
+        next(error); // Passa o erro para o middleware de tratamento de erros
+    }
 });
 
-app.get("/aluno/:id", async (req,res) => {
-    const id = req.params.id;
-    const aluno = await getAluno(id);
-    res.send(aluno);
+app.get("/aluno/:id", async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const aluno = await getAluno(id);
+        res.send(aluno);
+    } catch (error) {
+        console.error('Erro ao buscar aluno:', error);
+        next(error); // Passa o erro para o middleware de tratamento de erros
+    }
 });
 
-app.post("/alunos", async (req,res) => {
-    const { nome, idade, cidade } = req.body
-    const aluno = await createAluno(nome, idade, cidade)
-    res.status(201).send(aluno);
+app.post("/alunos", async (req, res, next) => {
+    try {
+        const { nome, idade, cidade } = req.body;
+        const aluno = await createAluno(nome, idade, cidade);
+        res.status(201).send(aluno);
+    } catch (error) {
+        console.error('Erro ao criar aluno:', error);
+        next(error); // Passa o erro para o middleware de tratamento de erros
+    }
+});
+
+// Middleware de tratamento de erros
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: 'Ocorreu um erro interno no servidor.',
+        error: err.message,
+    });
 });
 
 app.listen(8080, () => {
